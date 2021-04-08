86e5aa528d1248301db78092e6d8fd5c1473703a
monika <mphadtare@kennovation-services.com>
2021-04-08 12:14:11 +0530

    Change product view

--- models/models.py
+++ models/models.py
@@ -15,7 +15,7 @@ class ProductProduct(models.Model):
 class PurchaseOrderLine(models.Model):
     _inherit ='purchase.order.line'
 
-    value_usd = fields.Monetary(string="Cost in USD")
+    value_usd = fields.Integer(string="Cost in USD")
 
 class PurchaseOrder(models.Model):
     _inherit ='purchase.order'
